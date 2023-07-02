a93f0f201705d420788bb6bb36771eb59c6c2737
TryWebpage <138382527+TryWebpage@users.noreply.github.com>
2023-07-02 17:12:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 $(document).ready(function(){
-    let zoom = 1;
-    let left = 0;
-    let top = 0;
-    const moveIncrement = 10;
+  // Initialize zoom level and position
+  let zoom = 1;
+  let left = 0;
+  let top = 0;
 
-    // Only zoom when CTRL key is held down
-    $('.img-container').on('wheel', function(e) {
-        if (!e.ctrlKey) return; // Regular scroll if CTRL key is not held down
+  $('.img-container').on('wheel', function(e) {
+      e.preventDefault();
 
-        e.preventDefault();
+      // Zoom in/out based on wheel movement
+      zoom += e.originalEvent.deltaY * -0.01;
+      zoom = Math.min(Math.max(.125, zoom), 4);
+      
+      // Apply zoom
+      $('#image1, #image2').css('transform', `scale(${zoom})`);
+  });
 
-        zoom += e.originalEvent.deltaY * -0.01;
-        zoom = Math.min(Math.max(.125, zoom), 4);
-        
-        // Apply zoom
-        $('#image1, #image2').css('transform', `scale(${zoom})`);
-    });
+  $('.images').on('mousemove', function(e) {
+      if (zoom <= 1) return; // No need to move if not zoomed in
 
-    $('.images').on('mousemove', function(e) {
-        if (zoom <= 1) return; // No need to move if not zoomed in
+      let parentOffset = $(this).offset();
+      let relX = e.pageX - parentOffset.left;
+      let relY = e.pageY - parentOffset.top;
 
-        let parentOffset = $(this).offset();
-        let relX = e.pageX - parentOffset.left;
-        let relY = e.pageY - parentOffset.top;
+      // Set new position
+      left = (relX / $(this).width()) * 100;
+      top = (relY / $(this).height()) * 100;
 
-        // Set new position
-        left = (relX / $(this).width()) * 100;
-        top = (relY / $(this).height()) * 100;
-
-        // Apply position
-        $('#image1, #image2').css('left', -left + '%').css('top', -top + '%');
-    });
-
+      // Apply position
+      $('#image1, #image2').css('left', -left + '%').css('top', -top + '%');
+  });
 });
