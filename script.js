7f8493e185c2dd66bbdb2cdec7b7e5c6f7c5a9a6
TryWebpage <138382527+TryWebpage@users.noreply.github.com>
2023-07-02 17:10:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ $(document).ready(function(){
 
         e.preventDefault();
 
-        // Zoom in/out based on wheel movement
         zoom += e.originalEvent.deltaY * -0.01;
         zoom = Math.min(Math.max(.125, zoom), 4);
         
@@ -33,32 +32,4 @@ $(document).ready(function(){
         $('#image1, #image2').css('left', -left + '%').css('top', -top + '%');
     });
 
-    // Arrow keys to move image
-    $(document).keydown(function(e) {
-        if (zoom <= 1) return; // No need to move if not zoomed in
-
-        switch(e.which) {
-            case 37: // left
-                left = Math.min(left + moveIncrement, 100);
-                break;
-
-            case 39: // right
-                left = Math.max(left - moveIncrement, 0);
-                break;
-
-            case 38: // up
-                top = Math.min(top + moveIncrement, 100);
-                break;
-
-            case 40: // down
-                top = Math.max(top - moveIncrement, 0);
-                break;
-
-            default: return; // Exit if it's not an arrow key
-        }
-        e.preventDefault(); // Prevent the default action (scroll / move caret)
-
-        // Apply position
-        $('#image1, #image2').css('left', -left + '%').css('top', -top + '%');
-    });
 });
