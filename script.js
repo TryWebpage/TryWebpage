aa8778a7f261fce2a1db2c328667ab6e2a702386
TryWebpage <138382527+TryWebpage@users.noreply.github.com>
2023-07-02 17:02:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    // Initialize zoom level and position
     let zoom = 1;
     let left = 0;
     let top = 0;
-  
+    const moveIncrement = 10;
+
+    // Only zoom when CTRL key is held down
     $('.img-container').on('wheel', function(e) {
+        if (!e.ctrlKey) return; // Regular scroll if CTRL key is not held down
+
         e.preventDefault();
 
         // Zoom in/out based on wheel movement
@@ -15,16 +18,45 @@ $(document).ready(function(){
         $('#image1, #image2').css('transform', `scale(${zoom})`);
     });
 
-    $('.img-container').on('mousemove', function(e) {
+    $('.images').on('mousemove', function(e) {
         if (zoom <= 1) return; // No need to move if not zoomed in
 
-        let parentOffset = $(this).parent().offset();
+        let parentOffset = $(this).offset();
         let relX = e.pageX - parentOffset.left;
         let relY = e.pageY - parentOffset.top;
 
         // Set new position
         left = (relX / $(this).width()) * 100;
         top = (relY / $(this).height()) * 100;
+
+        // Apply position
+        $('#image1, #image2').css('left', -left + '%').css('top', -top + '%');
+    });
+
+    // Arrow keys to move image
+    $(document).keydown(function(e) {
+        if (zoom <= 1) return; // No need to move if not zoomed in
+
+        switch(e.which) {
+            case 37: // left
+                left = Math.min(left + moveIncrement, 100);
+                break;
+
+            case 39: // right
+                left = Math.max(left - moveIncrement, 0);
+                break;
+
+            case 38: // up
+                top = Math.min(top + moveIncrement, 100);
+                break;
+
+            case 40: // down
+                top = Math.max(top - moveIncrement, 0);
+                break;
+
+            default: return; // Exit if it's not an arrow key
+        }
+        e.preventDefault(); // Prevent the default action (scroll / move caret)
 
         // Apply position
         $('#image1, #image2').css('left', -left + '%').css('top', -top + '%');
